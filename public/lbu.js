c99421977598ff462e53c1ccfb939c60b2937bf2
Martin Leopold <m@martinleopold.com>
2020-01-14 17:54:25 +0100

    Added support to rotate preview image based on EXIF orientation

--- public/lbu.js
+++ public/lbu.js
@@ -131,14 +131,64 @@ export function setupUploadCounter(opts) {
   });
 }
 
+// https://gist.github.com/runeb/c11f864cd7ead969a5f0
+function arrayBufferToBase64( buffer ) {
+  var binary = ''
+  var bytes = new Uint8Array( buffer )
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode( bytes[ i ] )
+  }
+  return window.btoa( binary );
+}
+
+// https://gist.github.com/runeb/c11f864cd7ead969a5f0
+function getImageRotation( buffer ) {
+  var scanner = new DataView(buffer);
+  var idx = 0;
+  var value = 1; // Non-rotated is the default
+  if (buffer.length < 2 || scanner.getUint16(idx) != 0xFFD8) {
+    // Not a JPEG
+    return '';
+  }
+  idx += 2;
+  var maxBytes = scanner.byteLength;
+  while(idx < maxBytes - 2) {
+    var uint16 = scanner.getUint16(idx);
+    idx += 2;
+    switch(uint16) {
+      case 0xFFE1: // Start of EXIF
+        var exifLength = scanner.getUint16(idx);
+        maxBytes = exifLength - idx;
+        idx += 2;
+        break;
+      case 0x0112: // Orientation tag
+        // Read the value, its 6 bytes further out
+        // See page 102 at the following URL
+        // http://www.kodak.com/global/plugins/acrobat/en/service/digCam/exifStandard2.pdf
+        value = scanner.getUint16(idx + 6, false);
+        maxBytes = 0; // Stop scanning
+        break;
+    }
+  }
+  const rotation = {
+    1: 'rotate(0deg)',
+    3: 'rotate(180deg)',
+    6: 'rotate(90deg)',
+    8: 'rotate(270deg)'
+  };
+  if (!(value in rotation)) return '';
+  return rotation[value];
+}
 
 // Optional Callback is invoked every time an image is loaded
 // Returns: Promise, resolves when first image is loaded
 export async function setupImageSelect(opts, cb) {
   const defaults = {
     input: '#photo',
-    image: '#photo-display img',
+    image: '', // e.g.: '#photo-display img'
     background: '',
+    exifRotate: false,
   }
   opts = Object.assign({}, defaults, opts);
   let input = document.querySelector(opts.input);
@@ -149,17 +199,24 @@ export async function setupImageSelect(opts, cb) {
       if ( !file || !file.type.startsWith('image/') ) return;
       let reader = new FileReader();
       reader.onload = e => {
+        let buffer = e.target.result;
+        let dataURL = "data:" + file.type + ";base64," + arrayBufferToBase64(buffer);
+        let rotation = opts.exifRotate ? getImageRotation(buffer) : '';
+        
         if (opts.background) {
           let img = document.querySelector(opts.background);
-          img.style.setProperty('background-image', `url(${e.target.result})`, 'important');
-        } else {
+          img.style.setProperty('background-image', `url(${dataURL})`, 'important');
+          if (rotation) img.style.transform = rotation;
+        }
+        if (opts.image) {
           let img = document.querySelector(opts.image);
-          img.src = e.target.result;
+          img.src = dataURL;
+          if (rotation) img.style.transform = rotation;
         }
         resolve(e);
         if (cb instanceof Function) cb(e);
       };
-      reader.readAsDataURL(file);
+      reader.readAsArrayBuffer(file);
     });
   });
 }
